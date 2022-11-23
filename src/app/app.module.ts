99532e7789198bafbee52ab108c65ba4176a7639
zafuso <71095771+zafuso@users.noreply.github.com>
2022-11-23 07:09:08 +0100

    make basis top-banner and header components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopBannerComponent } from './header/top-banner/top-banner.component';
+import { HeaderComponent } from './header/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBannerComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
