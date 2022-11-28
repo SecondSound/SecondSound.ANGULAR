cda243860cb86d99d9cd08b70d356371a360f5c7
Lars Hanegraaf <lars.hanegraaf@cm.com>
2022-11-28 16:38:31 +0100

    add login/register form and basic styling

--- src/app/components/account/register/register.component.ts
+++ src/app/components/account/register/register.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import {ErrorStateMatcher} from "@angular/material/core";
+import {FormControl, FormGroupDirective, NgForm, Validators} from "@angular/forms";
+import {LoginErrorStateMatcher} from "../login/login.component";
+
+export class RegisterErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 
 @Component({
   selector: 'app-register',
@@ -6,10 +16,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  hide: boolean = true;
+
+  emailFormControl: FormControl = new FormControl('', [Validators.required, Validators.email]);
+  passwordFormControl: FormControl = new FormControl('', [Validators.required, Validators.minLength(6)]);
+  nameFormControl: FormControl = new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(50)]);
+
+  matcher: RegisterErrorStateMatcher = new RegisterErrorStateMatcher();
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  displayPassword() {
+    this.hide = !this.hide;
+  }
 }
