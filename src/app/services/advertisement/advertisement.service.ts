b9897c10ef6bbb95360cb747ad20c7006bbc1c52
Rob vd H <accounts.rob@mailbox.org>
2022-12-04 01:01:11 +0100

    Update advertisement.service.ts

--- src/app/services/advertisement/advertisement.service.ts
+++ src/app/services/advertisement/advertisement.service.ts
@@ -1,5 +1,5 @@
 import {Injectable, OnInit} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {environment} from "../../../environments/environment";
 import {AdvertisementModel} from "../../shared/models/advertisement-model.model";
 import {Observable} from "rxjs";
@@ -11,11 +11,6 @@ import {Observable} from "rxjs";
 export class AdvertisementService {
   private baseUrl = environment.BASE_URL;
   private apiVersion = environment.API_VERSION
-  private defaultConfig = {
-    withCredentials: false,
-    headers: {},
-    params: {}
-  }
 
   constructor(private http: HttpClient) {
   }
@@ -25,8 +20,9 @@ export class AdvertisementService {
   }
 
   public postAdvertisement(advertisement: AdvertisementModel) : Observable<any>{
+
     console.log(advertisement.title, advertisement.description, advertisement.price)
-    return this.http.post(this.baseUrl + "/api/" + this.apiVersion + "/advertisement", advertisement, this.defaultConfig);
+    return this.http.post(this.baseUrl + "/api/" + this.apiVersion + "/advertisement", advertisement);
   }
 
 
