8f98120dda0d4b322eab265b9f4d25da3f51e88b
zafuso <71095771+zafuso@users.noreply.github.com>
2022-12-29 19:21:58 +0100

    add confirm password

--- src/app/components/account/register/register-form/register-form.component.ts
+++ src/app/components/account/register/register-form/register-form.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import {Register} from "../../../../shared/models/register.model";
-import {FormControl, Validators} from "@angular/forms";
+import {
+  AbstractControl,
+  FormControl,
+  ValidationErrors,
+  ValidatorFn,
+  Validators
+} from "@angular/forms";
 import {FormErrorStateMatcher} from "../../../../shared/error-state-matcher/FormErrorStateMatcher";
 import {AuthManagementService} from "../../../../services/auth-management.service";
 
@@ -12,17 +18,20 @@ import {AuthManagementService} from "../../../../services/auth-management.servic
 export class RegisterFormComponent implements OnInit {
   hide: boolean = true;
   registrationStarted: boolean = false;
+  passwordsMatch: boolean = false;
   minLengthPassword: number = 8;
   minLengthName: number = 3;
   maxLengthFirstName: number = 50;
   maxLengthLastName: number = 80;
   email: string = '';
   password: string = '';
+  confirmPassword: string = '';
   firstName: string = '';
   lastName: string = '';
 
   emailFormControl: FormControl = new FormControl('', [Validators.required, Validators.email]);
   passwordFormControl: FormControl = new FormControl('', [Validators.required, Validators.minLength(this.minLengthPassword)]);
+  passwordConfirmFormControl: FormControl = new FormControl('', [Validators.required, Validators.minLength(this.minLengthPassword)]);
   firstNameFormControl: FormControl = new FormControl('', [Validators.required, Validators.minLength(this.minLengthName), Validators.maxLength(this.maxLengthFirstName)]);
   lastNameFormControl: FormControl = new FormControl('', [Validators.required, Validators.minLength(this.minLengthName), Validators.maxLength(this.maxLengthLastName)]);
 
@@ -51,6 +60,25 @@ export class RegisterFormComponent implements OnInit {
 
   getFormState(): boolean {
     return this.firstNameFormControl.valid && this.lastNameFormControl.valid &&
-      this.emailFormControl.valid && this.passwordFormControl.valid;
+      this.emailFormControl.valid && this.passwordFormControl.valid && this.passwordsMatch;
+  }
+
+  matchPasswordsValidator(): void {
+    if (this.passwordConfirmFormControl.hasError('passwordsDontMatch')) {
+      this.passwordConfirmFormControl.setErrors({'passwordsDontMatch': false})
+      this.passwordConfirmFormControl.updateValueAndValidity();
+    }
+
+    this.passwordsMatch = this.password === this.confirmPassword;
+    let errors:ValidationErrors = this.passwordConfirmFormControl.errors;
+
+    if (!this.passwordsMatch)
+      if (errors) {
+        errors['passwordsDontMatch'] = true;
+      } else {
+        errors = {'passwordsDontMatch': true};
+    }
+
+    this.passwordConfirmFormControl.setErrors(errors);
   }
 }
