85029c5ae372139c2fca719b6a2332d4a848f9bc
Shabular <43166019+Shabular@users.noreply.github.com>
2022-12-29 11:01:53 +0100

    chat fully functional
    todo: users in chat can open chat others not, if chat exist dont make new one open old, styling

--- src/app/services/chat/chat.service.ts
+++ src/app/services/chat/chat.service.ts
@@ -5,6 +5,7 @@ import {environment} from "../../../environments/environment";
 import {Observable} from "rxjs";
 import {AdvertisementDto} from "../../shared/models/AdvertisementDto";
 import {ChatDto} from "../../shared/models/ChatDto";
+import {Router} from "@angular/router";
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +14,8 @@ export class ChatService {
   private baseUrl = environment.BASE_URL;
   private apiVersion = environment.API_VERSION
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient,
+              private router: Router) {
   }
 
   public getAllChats() {
@@ -21,13 +23,14 @@ export class ChatService {
   }
 
   public newChat(id: number){
-    console.log(id)
 
     const data = {
           id: id
         };
 
-    return this.http.post(this.baseUrl + "/api/" + this.apiVersion + "/chat/create/" + id, data, {responseType: 'text' as 'json'}).subscribe();
+   return this.http.post<string>(this.baseUrl + "/api/" + this.apiVersion + "/chat/create/" + id, data, {responseType: 'text' as 'json'}).subscribe(data => {
+      this.router.navigate([this.baseUrl + "/api/" + this.apiVersion + "/chat-screen/" + data])});
+
   }
 
 
