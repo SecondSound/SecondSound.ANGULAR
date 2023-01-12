1e034008c5386c06771e7e23ce09bd4b38574835
Rob vd H <accounts.rob@mailbox.org>
2023-01-12 20:38:08 +0100

    set production url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  API_URL: "http://localhost:8080"
+  BASE_URL: "https://second-soundjava-production.up.railway.app",
+  API_VERSION: "v1.0",
+  JWT_TOKEN_KEY: 'second_sound_user'
 };
