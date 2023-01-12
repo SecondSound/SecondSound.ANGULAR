1b45efba3c35c1e84f06629ce4ac6c659381091d
Shabular <43166019+Shabular@users.noreply.github.com>
2023-01-12 18:47:16 +0100

    messenger is tested

---
+++ src/app/components/account/messenger/chat-screen/chat-screen.component.spec.ts
@@ -0,0 +1,62 @@
+import { ChatScreenComponent } from './chat-screen.component';
+import {NotifierModule} from "angular-notifier";
+import {AuthManagementService} from "../../../../services/auth-management.service";
+import {AuthService} from "../../../../services/auth.service";
+import {ChatService} from "../../../../services/chat/chat.service";
+import {HttpClientTestingModule, HttpTestingController} from "@angular/common/http/testing";
+import {CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA} from "@angular/core";
+import {of} from "rxjs";
+import {HttpClient} from "@angular/common/http";
+import {ComponentFixture, TestBed, waitForAsync} from "@angular/core/testing";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {CommonModule} from "@angular/common";
+import { RouterTestingModule } from '@angular/router/testing';
+
+describe('ChatScreenComponent', () => {
+  let httpClientSpy: jasmine.SpyObj<HttpClient>;
+  let chatService: ChatService;
+  let component: ChatScreenComponent;
+  let fixture: ComponentFixture<ChatScreenComponent>;
+
+  beforeEach(async () => {
+
+     let httpClientSpyObj = jasmine.createSpyObj('HttpClient', ['get']);
+
+    await TestBed.configureTestingModule({
+    imports: [
+            RouterTestingModule,
+            FormsModule,
+            ReactiveFormsModule,
+            NotifierModule,
+            HttpClientTestingModule,
+            CommonModule
+          ],
+     declarations: [ ChatScreenComponent ],
+     providers: [
+       AuthManagementService,
+       AuthService
+     ],
+     schemas: [CUSTOM_ELEMENTS_SCHEMA]
+    })
+    .compileComponents();
+
+      chatService = TestBed.inject(ChatService);
+      httpClientSpy = TestBed.inject(HttpClient) as jasmine.SpyObj<HttpClient>;
+    });
+
+
+  it('should create the chatScreenComponent', () => {
+      let fixture: ComponentFixture<ChatScreenComponent> = TestBed.createComponent(ChatScreenComponent);
+      let component: ChatScreenComponent = fixture.componentInstance;
+
+      fixture.detectChanges();
+      expect(component).toBeTruthy();
+    });
+
+});
+
+
+
+
+
+
--- src/app/components/account/messenger/chats/chats.component.spec.ts
+++ src/app/components/account/messenger/chats/chats.component.spec.ts
@@ -5,7 +5,9 @@ import {AuthService} from "../../../../services/auth.service";
 import {HttpClientTestingModule} from "@angular/common/http/testing";
 import {NotifierModule} from "angular-notifier";
 import {LoginResponse} from "../../../../shared/models/login-response.model";
-//import {ChatDto} from "../../../../shared/models/chatDto";
+import {ChatDto} from "../../../../shared/models/ChatDto";
+import {UserDto} from "../../../../shared/models/UserDto";
+import {AdvertisementDto} from "../../../../shared/models/AdvertisementDto";
 import {MatMenuModule} from "@angular/material/menu";
 import { ChatsComponent } from './chats.component';
 
@@ -14,6 +16,7 @@ import { ChatsComponent } from './chats.component';
 describe('ChatsComponent', () => {
   let component: ChatsComponent;
   let fixture: ComponentFixture<ChatsComponent>;
+
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -44,30 +47,65 @@ describe('ChatsComponent', () => {
 
   it('should show a chat', () => {
 
-  let user: LoginResponse =
-        {
-          firstName: 'Bart',
-          lastName: 'Grootoonk',
-          accessToken: null,
-          userId: null,
-          expirationDate: null
-        }
-//       let chat: ChatDto =
-//         {
-//           advertisement: null,
-//           sender: null,
-//           receiver: null
-//
-//         }
-//
-//       let chats : List<Chat>, () => {
-//         chat
-//       }
 
-//       component.chats = chat;
+      let advertisement :AdvertisementDto ={
+        id: 1,
+        title: 'instrument',
+        description: 'test description',
+        price: '200.00',
+        imgFile: 'https://www.testimage2.com',
+        seller: null,
+        subCategory: null,
+        saved: false
+      }
+
+      let sender : UserDto = {
+         id: 1,
+         firstName: "henk",
+         lastName: "peters",
+         street: "wilgenlaan",
+         imgFile: "geen",
+         houseNumber: 1,
+         postalCode: "4872ee",
+         city: "breda",
+         country: "nederland",
+         phoneNumber: "0654345678",
+         iban: "blah",
+         email: "henk@henk.com"
+      }
+
+       let receiver : UserDto = {
+               id: 2,
+               firstName: "piet",
+               lastName: "peters",
+               street: "wilgjeslaan",
+               imgFile: "none",
+               houseNumber: 2,
+               postalCode: "3748kk",
+               city: "etten-leur",
+               country: "nederland",
+               phoneNumber: "0654190678",
+               iban: "blah",
+               email: "piet@henk.com"
+            }
+
+      let chat: ChatDto =
+        {
+          id: 1,
+          advertisement: advertisement,
+          sender: sender,
+          receiver: receiver
+        }
+
+      console.log(chat);
+      let chats : ChatDto[] = [];
+
+      chats.push(chat);
+
+      component.chats = chats;
       fixture.detectChanges();
       let compiled = fixture.debugElement.nativeElement;
-      expect(compiled.textContent).toContain('');
+      expect(compiled.textContent).toContain('piet');
     });
 });
 
